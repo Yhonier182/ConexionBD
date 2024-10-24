701ed1708c33a44f9aa0b0a802126b0308774d3a
Yhonier182 <jhonier18249@gmail.com>
2024-10-24 07:22:24 -0500

    eliminar comentario

--- src/main/java/Vista/VentanaConsultaIndividual.java
+++ src/main/java/Vista/VentanaConsultaIndividual.java
@@ -346,5 +346,6 @@ public class VentanaConsultaIndividual extends JDialog implements ActionListener
             JOptionPane.showMessageDialog(null, "Error al registrar el usuario", "Error", JOptionPane.ERROR_MESSAGE);
         }
         limpiarVentana();
-    }  // mejorar las advertencias
+    }
+
 }
